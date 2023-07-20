ffcdfe07af6afa5861f81f5729769e0ad614b451
Bykov Denis <z.bykov.d@gmail.com>
2023-07-21 00:15:20 +0300

    dual_imu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
 use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
@@ -43,11 +43,9 @@ fn main() {
     .iter()
     .cloned()
     .cycle();
-
-    let mesurs = Arc::new(Mutex::new(Mesurs::default()));
-    {
-        mesurs.lock().unwrap().inst = Some(Instant::now());
-    }
+    let mut mesurs = Mesurs::default();
+    mesurs.inst = Some(Instant::now());
+    let mesurs = Arc::new(Mutex::new(mesurs));
     let mcl = mesurs.clone();
 
     ctrlc::set_handler(move || {
@@ -61,98 +59,68 @@ fn main() {
         println!("imu med: {}ms", m.imu_all[m.imu_all.len() / 2].as_millis());
         println!("imu min: {}ms, at {}", m.imu_min.unwrap().as_millis(), m.imu_min_count);
         println!("imu max: {}ms, at {}", m.imu_max.unwrap().as_millis(), m.imu_max_count);
-        println!("");
-        println!("gps count: {}", m.gps_count);
-        println!("gps avr: {}ms", m.gps_sum.as_millis() / m.gps_count as u128);
-        m.gps_all.sort();
-        println!("gps med: {}ms", m.gps_all[m.gps_all.len() / 2].as_millis());
-        println!("gps min: {}ms, at {}", m.gps_min.unwrap().as_millis(), m.gps_min_count);
-        println!("gps max: {}ms, at {}", m.gps_max.unwrap().as_millis(), m.gps_max_count);
+        // println!("");
+        // println!("gps count: {}", m.gps_count);
+        // println!("gps avr: {}ms", m.gps_sum.as_millis() / m.gps_count as u128);
+        // m.gps_all.sort();
+        // println!("gps med: {}ms", m.gps_all[m.gps_all.len() / 2].as_millis());
+        // println!("gps min: {}ms, at {}", m.gps_min.unwrap().as_millis(), m.gps_min_count);
+        // println!("gps max: {}ms, at {}", m.gps_max.unwrap().as_millis(), m.gps_max_count);
         exit(0);
     })
     .unwrap();
 
     let mut imu = Instant::now();
-    let mut gps = Instant::now();
+    // let mut gps = Instant::now();
 
     loop {
-        let mut v = vec![];
-        match port.read_until('\t' as u8, &mut v) {
-            Ok(_t) => {
+        // let mut v = vec![];
+        // match port.read_until('\t' as u8, &mut v) {
+        let mut v = [0u8; 4 * 12];
+        match port.read_exact(&mut v) {
+            Ok(_) => {
                 let mut m = mesurs.lock().unwrap();
-                let s = String::from_utf8_lossy(v.as_slice());
-                let ss = s.trim();
-                println!("{}", ss.color(colors.next().unwrap()));
-
-                if ss.starts_with("imu") {
-                    let imud = imu.elapsed();
-                    m.imu_all.push(imud);
-                    m.imu_sum += imud;
-                    m.imu_count += 1;
-                    m.imu_min = if m.imu_count < 3 {
-                        m.imu_min
-                    } else {
-                        match m.imu_min {
-                            Some(dur) => {
-                                // Some(dur.min(imud));
-                                if imud < dur {
-                                    m.imu_min_count = m.imu_count;
-                                    Some(imud)
-                                } else {
-                                    Some(dur)
-                                }
-                            }
-                            None => Some(imud),
-                        }
-                    };
-                    m.imu_max = match m.imu_max {
+                let v: Vec<f32> = v
+                    .chunks_exact(4)
+                    .map(|chunk| f32::from_le_bytes(chunk.try_into().expect(&format!("{:?}", chunk))))
+                    .collect();
+                // println!("{:?}", v);
+                let (f, s) = v.split_at(6);
+                println!("imu1 {:?}", f);
+                println!("imu2 {:?}", s);
+                let imud = imu.elapsed();
+                m.imu_all.push(imud);
+                m.imu_sum += imud;
+                m.imu_count += 1;
+                m.imu_min = if m.imu_count < 3 {
+                    m.imu_min
+                } else {
+                    match m.imu_min {
                         Some(dur) => {
                             // Some(dur.min(imud));
-                            if imud > dur {
-                                m.imu_max_count = m.imu_count;
+                            if imud < dur {
+                                m.imu_min_count = m.imu_count;
                                 Some(imud)
                             } else {
                                 Some(dur)
                             }
                         }
                         None => Some(imud),
-                    };
-                    imu = Instant::now();
-                } else {
-                    let gpsd = gps.elapsed();
-                    m.gps_sum += gpsd;
-                    m.gps_all.push(gpsd);
-                    m.gps_count += 1;
-                    m.gps_min = if m.gps_count < 3 {
-                        m.gps_min
-                    } else {
-                        match m.gps_min {
-                            Some(dur) => {
-                                // Some(dur.min(imud));
-                                if gpsd < dur {
-                                    m.gps_min_count = m.gps_count;
-                                    Some(gpsd)
-                                } else {
-                                    Some(dur)
-                                }
-                            }
-                            None => Some(gpsd),
+                    }
+                };
+                m.imu_max = match m.imu_max {
+                    Some(dur) => {
+                        // Some(dur.min(imud));
+                        if imud > dur {
+                            m.imu_max_count = m.imu_count;
+                            Some(imud)
+                        } else {
+                            Some(dur)
                         }
-                    };
-                    m.gps_max = match m.gps_max {
-                        Some(dur) => {
-                            // Some(dur.min(imud));
-                            if gpsd > dur {
-                                m.gps_max_count = m.gps_count;
-                                Some(gpsd)
-                            } else {
-                                Some(dur)
-                            }
-                        }
-                        None => Some(gpsd),
-                    };
-                    gps = Instant::now();
-                }
+                    }
+                    None => Some(imud),
+                };
+                imu = Instant::now();
             }
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
             Err(e) => eprintln!("{:?}", e),
